c5ad7afc31a488d496ab32e318d2141685fc526f
kazeneza-zephilin <kzephilin2@gmail.com>
2024-08-28 11:37:44 +0200

    added range input refactored code to ensure efficient and fix scratch.

--- scripts.js
+++ scripts.js
@@ -1,40 +1,24 @@
-const container = document.querySelector(".container");
-const canva = document.createElement("div");
-canva.style.cssText =
-  "width:320px ;height:320px; display:flex; flex-wrap:wrap; justify-content: center; align-item:center; margin: 100px auto";
-container.appendChild(canva);
+document.addEventListener("DOMContentLoaded", (event) => {
+  const rangeInput = document.querySelector("#rangeInput");
+  const rangeValue = document.querySelector("#rangeValue");
+  const container = document.querySelector(".container");
+  const canva = document.createElement("div");
 
-for (let j = 0; j < 16 * 16; j++) {
-  const pixel = document.createElement("div");
-  pixel.style.cssText = "background-color: yellow; width:20px; height:20px";
-  pixel.classList.add("square");
-  canva.appendChild(pixel);
-}
+  canva.style.cssText =
+    "width:320px ;height:320px; display:flex; flex-wrap:wrap; justify-content: center; align-item:center; margin: 100px auto";
+  container.appendChild(canva);
 
-promptBtn = document.createElement("button");
-promptBtn.style.cssText =
-  "width:75px; height: 25px; margin: 50px 500px";
-promptBtn.textContent = "add pixel";
-container.appendChild(promptBtn);
+  function getNewGrid(size) {
+    canva.innerHTML = "";
+    const pixelSize = 320 / size;
 
-function getPixel() {
-  const popupDialog = document.createElement("div");
-  const pixelNUmber = (popupDialog.textContent = prompt(
-    "enter the number of pexel"
-  ));
-  return pixelNUmber;
-}
+    for (let i = 0; i < size * size; i++) {
+      const pixel = document.createElement("div");
+      pixel.style.cssText = `background-color: yellow; width:${pixelSize}px; height:${pixelSize}px`;
+      pixel.classList.add("square");
+      canva.appendChild(pixel);
+    }
 
-function getNewGrid() {
-  const pixelCount = getPixel();
-  const numberOfDiv = parseInt(pixelCount) || 16;
-  canva.innerHTML = "";
-  const pixelSize = 320 / numberOfDiv;
-  for (let i = 0; i < numberOfDiv * numberOfDiv; i++) {
-    const pixel = document.createElement("div");
-    pixel.style.cssText = `background-color: yellow; width:${pixelSize}px; height:${pixelSize}px`;
-    pixel.classList.add("square");
-    canva.appendChild(pixel);
     const squares = document.querySelectorAll(".square");
     squares.forEach((square) => {
       square.addEventListener("mouseover", (event) => {
@@ -42,25 +26,18 @@ function getNewGrid() {
       });
     });
   }
-}
 
-promptBtn.addEventListener("click", (event) => {
-  getNewGrid();
+  function handleInputChange() {
+    const newSize = parseInt(rangeInput.value, 10);
+    rangeValue.textContent = newSize;
+    getNewGrid(newSize);
+  }
+
+  handleInputChange();
+
+  rangeInput.addEventListener("input", handleInputChange);
+
+  function changeBackgroundColor(elem) {
+    elem.style.backgroundColor = "red";
+  }
 });
-
-const squares = document.querySelectorAll(".square");
-squares.forEach((square) => {
-  square.addEventListener("mouseover", (event) => {
-    changeBackgroundColor(square);
-  });
-});
-function changeBackgroundColor(elem) {
-  elem.style.backgroundColor = "red";
-}
-
-function removeDivs() {
-  const squares = document.querySelectorAll(".square");
-  squares.forEach((square) => {
-    square.parentNode.removeChild(square);
-  });
-}
